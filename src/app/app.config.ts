455abffaa3c5a30348d696f397e1003629c965ad
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-02-02 21:00:05 +0700

    16. View Transition API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [    
-    provideRouter(routes,withViewTransitions({    
-      onViewTransitionCreated(transitionInfo) {
-        console.log(transitionInfo);
-      }
-    }))
-    ]
+  providers: [
+    provideRouter(
+      routes, 
+      withViewTransitions(),
+      withComponentInputBinding()
+    ),  
+    provideAnimations(),  
+  ]
 };
--- src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -8,11 +8,9 @@ import { TitleComponent } from '@shared/title/title.component';
   imports: [CommonModule, TitleComponent],
   template: `
     <app-title title="View Transition 2"></app-title>
-    <section class="flex justify-start">
-      <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300">
-      <div class="bg-blue-500 w-56 h-56">
-
-      </div>
+    <section class="flex justify-end">
+      <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300" style="view-transition-name: hero1">
+      <div class="bg-blue-500 w-56 h-56" style="view-transition-name: hero2"></div>
     </section>
   `
 })
